8667ae83c152933c5b35089dd51435dac08d8370
fedesilva <fede.silva@gmail.com>
2014-11-12 23:19:05 -0200

    Refactor Stub code and complete tests
    + El código stub ahora vive en un objeto
    + Se declaran un par de types nuevos y un case class
    + Se renombran los archivos.
    
    SORRY!

--- src/main/scala/FinancialReader/FinancialTool.scala
+++ src/main/scala/FinancialReader/FinancialTool.scala
@@ -1,13 +1,19 @@
+import java.text.SimpleDateFormat
 import java.util.Date
 
 /** XXX
  * Created by f on 11/12/14.
  */
-object FinancialReader {
+object FinancialTool {
 
-  type Row      = Map[String, Double]
+
   type Sym      = String
   type SymValue = (Sym, Double)
+
+  type Column = (String, Double)
+  case class Row(date: Date, columns: Map[String, Double])
+
+  val columnNames = Seq("Open","High","Low","Close","Volume", "AdjClose")
 
   def findFiles(root: String, symbols: Set[Sym]): Seq[String] = ???
 
@@ -15,9 +21,13 @@ object FinancialReader {
 
   def parseLine(line:String): Row  = ???
 
-  //def relevantDate(row: Row): Boolean = ???
-  //def dateFromString
-
   def query(symbols: Seq[Sym], dates: Seq[Date], col: String): Map [Date, Seq[SymValue]] = ???
+
+
+  private def dateFromString(date: String): Date = {
+    val simpleDateFormat = new SimpleDateFormat("YYYY-MM-DD")
+    simpleDateFormat.parse(date)
+  }
+
 
 }
